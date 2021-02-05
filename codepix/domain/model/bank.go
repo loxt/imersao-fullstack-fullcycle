e37cb7966f33a6fb9e4b51e473b3295635092739
loxt <emannuelloxt@hotmail.com>
2021-02-04 21:47:59 -0400

    chore(WIP): base and bank model

---
+++ codepix/domain/model/bank.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+	"time"
+)
+
+type Bank struct {
+	Base `json:"base" valid:"required"`
+	Code string `json:"code,omitempty" valid:"notnull"`
+	Name string `json:"name,omitempty" valid:"notnull"`
+}
+
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: "",
+		Name: "",
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+	bank.UpdatedAt = time.Now()
+
+	err := bank.isValid()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &bank, nil
+}
